31614318641477073492728d3e74d5fe698c75ed
Dayeon Park <ddggs@naver.com>
2023-08-16 23:49:41 +0900

    [boj] 3003

---
+++ boj/java/Boj_3003.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Boj_3003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println((1 - scanner.nextInt()) + " " + (1 - scanner.nextInt()) + " " + (2 - scanner.nextInt()) + " " + (2 - scanner.nextInt()) + " " + (2 - scanner.nextInt()) + " " + (8 - scanner.nextInt()));
+    }
+}
