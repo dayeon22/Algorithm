f696fe776664d8d45fd47a5fa708568ed5b31299
Dayeon Park <ddggs@naver.com>
2023-08-20 23:27:02 +0900

    [boj] 11478

---
+++ boj/java/Boj_11478.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Boj_11478 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Set<String> set = new HashSet<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            for (int j = i + 1; j <= str.length(); j++) {
+                set.add(str.substring(i, j));
+            }
+        }
+        System.out.println(set.size());
+    }
+}
