c3b4e007f8fe2bf94b9fa885e4e653b42eaa13f4
Dayeon Park <ddggs@naver.com>
2023-09-08 23:49:49 +0900

    [boj] 11654

---
+++ boj/java/Boj_11654.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Boj_11654 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println((int)scanner.nextLine().charAt(0));
+    }
+}
